225e8efff62956085d86cf7c13e983e760a8e406
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2017-06-18 13:52:46 -0400

    Completed: menu, and front page In Progress: backend to front end projects items TODO: contact page, footer, and routing

---
+++ src/app/project-page/projects.ts
@@ -0,0 +1,13 @@
+export class Projects {
+	public name: String;
+	public description: String;
+	public link: String;
+	public repo_link: String;
+	public blog_link: String;
+	public collaborators: String[];
+	public idea_creators: String[];
+	public image_description: String;
+	public image_path: String;
+	public start_date: String;
+	public end_date: String;
+}
---
+++ src/assets/php/class/project.php
@@ -0,0 +1,24 @@
+<?php
+
+ class Project{
+ 	public $name;
+ 	public $description;
+ 	public $link;
+ 	public $repo_link;
+ 	public $blog_link;
+ 	public $image_path;
+ 	public $image_description;
+ 	public $collaborators = array();
+ 	public $idea_creators = array();
+ 	public $start_date;
+ 	public $end_date;
+
+
+
+
+ }
+
+
+
+
+?>
